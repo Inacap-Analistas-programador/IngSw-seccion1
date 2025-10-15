2fc3411c50dd2e55608ff1acf3ea23a9c00f2369
Argonozo <elmecaniqueador@gmail.com>
2025-10-15 11:41:03 -0300

    style(pytest): Reformat DJANGO_SETTINGS_MODULE setup in conftest
    The `os.environ.setdefault` call for `DJANGO_SETTINGS_MODULE` in `backend/conftest.py`
    has been reformatted. This change improves readability and aligns with
    code style guidelines, without altering the functionality.

--- backend/conftest.py
+++ backend/conftest.py
@@ -2,7 +2,10 @@ import os
 import django
 from django.conf import settings
 
+
 def pytest_configure():
     if not settings.configured:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scouts_platform.settings.development')
+        os.environ.setdefault(
+            "DJANGO_SETTINGS_MODULE", "scouts_platform.settings.development"
+        )
         django.setup()
