9fd15db4f4c3d0d191d68ca5d47919db19da14a3
Kingshiba204 <228562973+Nachito-King@users.noreply.github.com>
2025-10-26 23:34:16 -0300

    Update CardTitle.vue

--- frontend/src/components/ui/card/CardTitle.vue
+++ frontend/src/components/ui/card/CardTitle.vue
@@ -1,21 +1,9 @@
-<script setup lang="ts">
-import { cn } from '@/lib/utils'
-import type { HTMLAttributes } from 'vue';
-
-const props = defineProps<{
-  class?: HTMLAttributes['class']
-}>()
-</script>
-
 <template>
-  <h3
-    :class="
-      cn(
-        'text-2xl font-semibold leading-none tracking-tight',
-        props.class,
-      )
-    "
-  >
+  <h3 :class="cn('text-2xl font-semibold leading-none tracking-tight', $attrs.class)" v-bind="$attrs">
     <slot />
   </h3>
 </template>
+
+<script setup lang="ts">
+import { cn } from '@/lib/utils'
+</script>
