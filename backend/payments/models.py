7ebfa46377b401e7b78c0a947d8510e63e23ea6d
k4inos1 <ricardo.sanhueza09@inacapmail.cl>
2025-11-01 07:06:33 -0300

    feat: Refactorizar el projecto y los modelos

--- backend/payments/models.py
+++ backend/payments/models.py
@@ -7,23 +7,14 @@ from common.models import PersonaCurso
 
 
 class PagoPersona(models.Model):
-    id = models.DecimalField(
-        db_column="pap_id", max_digits=10, decimal_places=0, primary_key=True
-    )
-    persona = models.ForeignKey(Persona, on_delete=models.RESTRICT, db_column="per_id")
-    curso = models.ForeignKey(Curso, on_delete=models.RESTRICT, db_column="cur_id")
-    usuario = models.ForeignKey(Usuario, on_delete=models.RESTRICT, db_column="usu_id")
-    fecha_hora = models.DateTimeField(db_column="pap_fecha_hora")
-    tipo = models.IntegerField(db_column="pap_tipo")
-    valor = models.DecimalField(db_column="pap_valor", max_digits=21, decimal_places=6)
-    estado = models.IntegerField(db_column="pap_estado")
-    observacion = models.CharField(
-        db_column="pap_observacion", max_length=100, blank=True, null=True
-    )
-
-    class Meta:
-        db_table = "pago_persona"
-
+    id = models.DecimalField(max_digits=10, decimal_places=0, primary_key=True)
+    persona = models.ForeignKey(Persona, on_delete=models.RESTRICT)
+    curso = models.ForeignKey(Curso, on_delete=models.RESTRICT)
+    usuario = models.ForeignKey(Usuario, on_delete=models.RESTRICT)
+    fecha_hora = models.DateTimeField()
+    tipo = models.IntegerField()
+    valor = models.DecimalField(max_digits=21, decimal_places=6)
+    observacion = models.CharField(max_length=100, blank=True, default='')
 
 class PagoCambioPersona(models.Model):
     id = models.DecimalField(
@@ -41,45 +32,23 @@ class PagoCambioPersona(models.Model):
 
 
 class Prepago(models.Model):
-    id = models.DecimalField(
-        db_column="ppa_id", max_digits=10, decimal_places=0, primary_key=True
-    )
-    persona = models.ForeignKey(Persona, on_delete=models.RESTRICT, db_column="per_id")
-    curso = models.ForeignKey(Curso, on_delete=models.RESTRICT, db_column="cur_id")
-    pago_persona = models.ForeignKey(
-        PagoPersona,
-        on_delete=models.RESTRICT,
-        db_column="pap_id",
-        blank=True,
-        null=True,
-    )
-    valor = models.DecimalField(db_column="ppa_valor", max_digits=21, decimal_places=6)
-    observacion = models.TextField(db_column="ppa_observacion", blank=True, null=True)
-    vigente = models.BooleanField(db_column="ppa_vigente")
-
-    class Meta:
-        db_table = "prepago"
-
+    id = models.DecimalField(max_digits=10, decimal_places=0, primary_key=True)
+    persona = models.ForeignKey(Persona, on_delete=models.RESTRICT)
+    curso = models.ForeignKey(Curso, on_delete=models.RESTRICT)
+    pago_persona = models.ForeignKey(PagoPersona, on_delete=models.RESTRICT, blank=True, default=0)
+    valor = models.DecimalField(max_digits=21, decimal_places=6)
+    observacion = models.TextField(blank=True, default='')
+    vigente = models.BooleanField()
 
 class ComprobantePago(models.Model):
-    id = models.DecimalField(
-        db_column="cpa_id", max_digits=10, decimal_places=0, primary_key=True
-    )
-    usuario = models.ForeignKey(Usuario, on_delete=models.RESTRICT, db_column="usu_id")
-    persona_curso = models.ForeignKey(
-        PersonaCurso, on_delete=models.RESTRICT, db_column="pec_id"
-    )
-    concepto_contable = models.ForeignKey(
-        ConceptoContable, on_delete=models.RESTRICT, db_column="coc_id"
-    )
-    fecha_hora = models.DateTimeField(db_column="cpa_fecha_hora")
-    fecha = models.DateField(db_column="cpa_fecha")
-    numero = models.IntegerField(db_column="cpa_numero")
-    valor = models.DecimalField(db_column="cpa_valor", max_digits=21, decimal_places=6)
-
-    class Meta:
-        db_table = "comprobante_pago"
-
+    id = models.DecimalField(max_digits=10, decimal_places=0, primary_key=True)
+    usuario = models.ForeignKey(Usuario, on_delete=models.RESTRICT)
+    persona_curso = models.ForeignKey(PersonaCurso, on_delete=models.RESTRICT)
+    concepto_contable = models.ForeignKey(ConceptoContable, on_delete=models.RESTRICT)
+    fecha_hora = models.DateTimeField()
+    fecha = models.DateField()
+    numero = models.IntegerField()
+    valor = models.DecimalField(max_digits=21, decimal_places=6)
 
 class PagoComprobante(models.Model):
     id = models.DecimalField(
