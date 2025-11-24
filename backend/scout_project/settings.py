03dc4cad72bf3543083a9eb2d0bb3f326c72216c
k4inos1 <ricardo.sanhueza09@inacapmail.cl>
2025-11-24 12:17:37 -0300

    Fix: Restore 'django.contrib.auth' to INSTALLED_APPS in settings.py

--- backend/scout_project/settings.py
+++ backend/scout_project/settings.py
@@ -39,8 +39,8 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     "django.contrib.admin",
-    "django.contrib.auth",
     "django.contrib.contenttypes",
+    "django.contrib.auth",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
